0c741ce4dda57efae312aca9aa16fc8ab6309f96
Tommy Bishop <bishop.thomas@gmail.com>
2012-03-28 10:17:06 -0700

    add prolog examples

---
+++ prolog/food.pl
@@ -0,0 +1,13 @@
+food_type(velveeta, cheese).
+food_type(ritz, cracker).
+food_type(spam, meat).
+food_type(sausage, meat).
+food_type(jolt, soda).
+food_type(twinkie, dessert).
+
+flavor(sweet, dessert).
+flavor(sweet, soda).
+flavor(savory, meat).
+flavor(savory, cheese).
+
+food_flavor(X, Y) :- food_type(X, Z), flavor(Y, Z).
